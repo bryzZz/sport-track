9c57b3fdaf2c2c4c49fc354ea0c3fae732109d46
bryzZz <tebloev.nikita@mail.ru>
2026-03-04 14:37:26 +0800

    PWA first try

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -2,10 +2,52 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 import svgr from "vite-plugin-svgr";
 
 export default defineConfig({
-  plugins: [tailwindcss(), svgr(), react()],
+  plugins: [
+    tailwindcss(),
+    svgr(),
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+
+      // отключаем precache
+      workbox: {
+        globPatterns: [],
+      },
+
+      manifest: {
+        name: "Sport Track",
+        short_name: "SportTrack",
+        description: "MVP для отслеживания прогресса тренировок",
+        start_url: "/",
+        display: "standalone",
+        orientation: "portrait",
+        background_color: "#ffffff",
+        theme_color: "#16a34a",
+        icons: [
+          {
+            src: "/pwa-icons/icon-192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/pwa-icons/icon-512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "/pwa-icons/icon-512-maskable.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       assets: fileURLToPath(new URL("./src/assets", import.meta.url)),
